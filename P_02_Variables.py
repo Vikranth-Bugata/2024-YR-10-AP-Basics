4008aaa926e3e9318a78191896dc19adcd561810
Vikranth-Bugata <bugatav3188@masseyhigh.school.nz>
2024-07-24 09:34:58 +1200

    Variable Basics

---
+++ P_02_Variables.py
@@ -0,0 +1,13 @@
+# code to learn about variables...
+
+# Variables can be a string (not the speech marks)
+greeting = "hello world"
+
+# Variables can be a number
+fav_number = 7
+num_string = "5"
+
+#Constants are uppercase
+PI = 3.14
+
+a_01_my_num = 7
